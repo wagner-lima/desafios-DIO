1c1c8d6f2e734932be6d750baf941ca71a511722
Wagner Filho <wagfilho@hotmail.com.br>
2021-09-18 14:02:42 -0300

    Solved more code challenges.

---
+++ C#/2. Introdução a Programação com .NET/3. Coxinha de Bueno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Classe
+{
+    static void Main()
+    {
+        string[] line = Console.ReadLine().Split(" ");
+
+        float A = Int32.Parse(line[0]);
+        float B = Int32.Parse(line[1]);
+
+        float total = A / B;
+
+        if (A % B != 0)
+        {
+            Console.WriteLine(Math.Round(total, 2).ToString("0.00"));
+        }
+        else
+        {
+            Console.WriteLine(total.ToString("0.00"));
+        }
+    }
+}
