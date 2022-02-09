f05d1b85be75411ed7f954d6342027bd2fac5a07
Wagner Lima <wagner.rlimafilho@gmail.com>
2022-02-08 21:14:28 -0300

    Adicionados certificados de conclusão de cursos

---
+++ C#/6. Desafios iniciais - GFT Start #4 .NET/2. Quadrado e ao Cubo/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Desafio
+ * Você terá o desafio de escrever 
+ * um programa que leia um valor 
+ * inteiro N (1 < N < 1000). 
+ * Este N é a quantidade de linhas 
+ * de saída que serão apresentadas na execução do programa.
+ * 
+ * Entrada
+ * O arquivo de entrada contém um número inteiro positivo N.
+ * 
+ * Saída
+ * Imprima a saída conforme o exemplo fornecido.
+ * 
+ * Exemplo de Entrada
+ * 5
+ * 
+ * Exemplo de Saída
+ * 1 1 1
+ * 2 4 8
+ * 3 9 27
+ * 4 16 64
+ * 5 25 125
+ */
+
+using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n != null)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine($"{i} {i * i} {i * i * i}");
+                }
+            }
+        }
+    }
+}
